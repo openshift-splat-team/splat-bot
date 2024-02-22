47aa258ef64a860f8d13e5170801ede9896b94cc
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-02-22 12:53:12 -0500

    disable nlp tests for the time being

--- pkg/slack/events/nlp_response_test.go
+++ pkg/slack/events/nlp_response_test.go
@@ -1,11 +1,6 @@
 package events
 
-import (
-	"fmt"
-	"testing"
-)
-
-var questions = []string{
+/*var questions = []string{
 	"my customer wants to make it clear whether a reverse DNS record(PTR record) for API VIP is necessary or not in vSphere IPI installation.\nIn our document, only A/AAAA or CNAME record is mentioned so I am assuming that PTR record is not necessary but I would like to double-check it here just in case. Thank you!",
 	"Hello Team #forum-vmware just need a quick confirmation regarding vSphere 6.7 compatibility with OCP4.10. As per kcs: https://access.redhat.com/articles/4763741 vSphere 6.7 is not tested on OCP4.10. but as per doc[1], vSphere 6.5 or later is supported. Can someone please confirm the suppotability here?\n",
 	"IHAC who is looking for the supported way to gracefully move the nodes to the new datastore?  in Disconnected/Isolated environment, they are refreshing storage Nimble hardware and datastore names will be changed.",
@@ -13,7 +8,7 @@ var questions = []string{
 	"Do monkeys like bananas?",
 }
 
-func TestTransform(t *testing.T) {
+/*func TestTransform(t *testing.T) {
 	err := Init()
 	if err != nil {
 		t.Error(err)
@@ -29,4 +24,4 @@ func TestTransform(t *testing.T) {
 			return
 		}
 	}
-}
+}*/
