b2b3e2475ca627441b167de2815813db4685f432
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2024-03-19 12:29:40 -0400

    move vSphere articles to their own package

--- pkg/knowledge/vsphere/vsphere67.go
+++ pkg/knowledge/vsphere/vsphere67.go
@@ -1,14 +1,15 @@
-package knowledge
+package vsphere
 
 import (
-	"github.com/openshift-splat-team/splat-bot/pkg/commands"
+	"github.com/openshift-splat-team/splat-bot/data"
+	"github.com/openshift-splat-team/splat-bot/pkg/util"
 )
 
-var VSphere67TopicAttributes = Knowledge{
-	Attributes: commands.Attributes{
-		MessageOfInterest: func(args []string, attribute commands.Attributes) bool {
-			argMap := normalizeTokens(args)
-			return tokensPresentOR(argMap, "vsphere", "vmware", "vcenter") && tokensPresentAND(argMap, "6.7")
+var VSphere67TopicAttributes = data.Knowledge{
+	Attributes: data.Attributes{
+		MessageOfInterest: func(args []string, attribute data.Attributes) bool {
+			argMap := util.NormalizeTokens(args)
+			return util.TokensPresentOR(argMap, "vsphere", "vmware", "vcenter") && util.TokensPresentAND(argMap, "6.7")
 		},
 	},
 	MarkdownPrompt: `vSphere 6 - 6.7 is end of life. Unless a customer has a VMware extended support agreement and a RH support exception OpenShift running on that version is not supported.
